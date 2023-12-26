060f9545c1262dbb511c057a644df8959c72e5ff
jumasaet <jumasaet@espol.edu.ec>
2023-12-26 15:39:36 -0500

    x

--- devel/setup.sh
+++
@@ -1,1 +0,0 @@
-/home/juan/moveit_ws/devel/.private/catkin_tools_prebuild/setup.sh
--- devel/share/robotic_arm/cmake/robotic_armConfig-version.cmake
+++
@@ -1,1 +0,0 @@
-/home/juan/moveit_ws/devel/.private/robotic_arm/share/robotic_arm/cmake/robotic_armConfig-version.cmake
